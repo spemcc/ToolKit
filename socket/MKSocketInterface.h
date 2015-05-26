b1bbea44ee547a2d78b9aeae9e46d907b9e179a0
Spencer McCreary <spencermccreary@gmail.com>
2015-05-26 15:49:41 -0400

    refactoring

--- socket/MKSocketInterface.h
+++ socket/MKSocketInterface.h
@@ -15,16 +15,16 @@ class MKSocketInterface {
     MKSocketInterface() {}
     virtual ~MKSocketInterface() {}
 
-    virtual bool Read(std::vector<uint8_t>* output,int numBytes) = 0;
+    virtual bool Read(std::vector<uint8_t>* output,int num_bytes) = 0;
     virtual bool Write(const std::vector<uint8_t>& data) = 0;
 
-    virtual bool Bind(uint16_t portNum) = 0; //Server Only
+    virtual bool Bind(uint16_t port_num) = 0; //Server Only
     virtual bool Listen() = 0;               //Server Only
     virtual int  Accept() = 0;                //Server Only
-    virtual bool Connect(std::string ip, uint16_t portNum, bool IPV6=false) =0; //Client Only
+    virtual bool Connect(std::string ip, uint16_t port_num, bool IPV6=false) =0; //Client Only
 
     virtual bool Close() = 0;
-    virtual void SetSocket(int socketfd) = 0;
+    virtual void SetSocket(int socket_fd) = 0;
 
   private:
 
